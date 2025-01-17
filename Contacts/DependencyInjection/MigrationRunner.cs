99d3f180222b7181eb9717db10052c0e904fa44b
Doruk Katiç <dorukkatic@outlook.com>
2025-01-18 00:22:08 +0300

    Run db migrations on startup on development env

---
+++ Contacts/DependencyInjection/MigrationRunner.cs
@@ -0,0 +1,17 @@
+﻿using Contacts.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Contacts.DependencyInjection;
+
+public static class MigrationRunner
+{
+    public static void RunDbMigrations(this IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var services = scope.ServiceProvider;
+        
+        var context = services.GetRequiredService<ContactsDbContext>();
+        context.Database.Migrate(); // Applies pending migrations
+    }
+}
